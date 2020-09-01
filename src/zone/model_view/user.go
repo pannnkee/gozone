f404c45ffda67f596fd045336bf9276c2c508cf3
潘科 <522240909@qq.com>
2020-09-02 00:53:12 +0800

    [use vue]

--- src/zone/model_view/user.go
+++ src/zone/model_view/user.go
@@ -2,8 +2,8 @@ package model_view
 
 type User struct {
 	Id             int64  `json:"id"`
-	UserName       string `json:"user_name"`
+	UserName       string `json:"username"`
 	PassWord       string `json:"password"`
-	RepeatPassword string `json:"repeat_password"`
+	RepeatPassword string `json:"repeatpassword"`
 	Email          string `json:"email"`
 }
