380e419e3e81a189fae872c3a31b16cd18300938
潘科 <522240909@qq.com>
2020-12-23 09:51:51 +0800

    [.]

--- src/zone/model_view/comment.go
+++ src/zone/model_view/comment.go
@@ -6,5 +6,5 @@ type CommentWeb struct {
 	Content       string `json:"content"`
 	ArticleId     int64  `json:"article_id"`
 	ReplyFatherID int64  `json:"reply_father_id"` //回复二级评论 需要定位是在哪一个一级评论
-	RepUserID     int64  `json:"rep_user_id"` //我回复 楼层用户的ID
+	RepUserID     int64  `json:"rep_user_id"`     //我回复 楼层用户的ID
 }
--- src/zone/model_view/user.go
+++ src/zone/model_view/user.go
@@ -2,11 +2,11 @@ package model_view
 
 // 用户
 type User struct {
-	Id             int64  `json:"id"`
-	UserName       string `json:"username"`
-	PassWord       string `json:"password"`
-	RepeatPassword string `json:"repeat_password"`
-	Email          string `json:"email"`
-	NewPassword    string `json:"new_password"`
-	VerifyCode     string `json:"verify_code"`
+	Id             int64  `json:"id"`				// 用户id
+	UserName       string `json:"username"`			// 用户姓名
+	PassWord       string `json:"password"`			// 密码
+	RepeatPassword string `json:"repeat_password"`	// 重复密码
+	Email          string `json:"email"`			// 邮箱
+	NewPassword    string `json:"new_password"`		// 新密码
+	VerifyCode     string `json:"verify_code"`		// 验证码
 }
